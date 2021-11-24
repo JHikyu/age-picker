f29f777e0f26576044eaf3732a94fad8d7a5942b
Justin <justin.rost2000@gmail.com>
2021-11-24 22:20:19 +0100

    Added all sites

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ require('dotenv').config();
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', (req, res) => {
     res.render('index.pug');
+});
+app.get('/picker', (req, res) => {
+    res.render('picker.pug');
+});
+app.get('/randomizer', (req, res) => {
+    res.render('randomizer.pug');
+});
+app.get('/trial', (req, res) => {
+    res.render('trial.pug');
+});
+app.get('/news', (req, res) => {
+    res.render('news.pug');
+});
+app.get('/faq', (req, res) => {
+    res.render('faq.pug');
 });
 
 
